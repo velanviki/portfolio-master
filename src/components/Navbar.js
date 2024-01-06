865908b83f5de54753e8213334a3257d193496e5
velan <velanvicky01@gmail.com>
2024-01-06 19:09:46 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import "../styles/Navbar.css";
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleNavClose = () => {
+    setIsOpen(false);
+  };
   return (
     <div className="Navbar">
       <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#skill">Skills</a>
-        <a href="#project">Project</a>
-        <a href="#resume">My Journey</a>
-
-        <a href="#contact">Contact</a>
+        <a href="#home" onClick={handleNavClose}>Home</a>
+        <a href="#about" onClick={handleNavClose}>About</a>
+        <a href="#skill" onClick={handleNavClose}>Skills</a>
+        <a href="#project" onClick={handleNavClose}>Project</a>
+        <a href="#resume" onClick={handleNavClose}>My Journey</a>
+        <a href="#contact" onClick={handleNavClose}>Contact</a>
       </div>
       <img
         className="suit"
